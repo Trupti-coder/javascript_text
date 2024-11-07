4c2b6bdafcafecdbf954b9995d3074eada728c18
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-07 16:25:43 +0530

    Write fun for checking Array contain duplicate element

--- vowels.js
+++ vowels.js
@@ -1,3 +1,18 @@
-function isDuplicate(){
+function isDuplicate(arr){
+    let result="";
+    for(let i=0;i<arr.length;i++){
+        var isDuplicate=false;
+        for(let j=0;j<result.length;j++){
+            if(arr[i]===result[j]){
+                isDuplicate=true;
+                break;
+
+            }
+        }
+        if(!isDuplicate){
+            result.push(arr[i]);
+        }
+    }
+    return result;
     
 }

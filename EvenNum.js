540e601f3d79d97debff6d24e03b1f40178f02ba
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-07 16:32:54 +0530

    Even Number

---
+++ EvenNum.js
@@ -0,0 +1,11 @@
+function checkEvenNum(){
+    let n=5;
+    for(let i=0;i<n;i++){
+        if(i%2==0){
+            console.log("even number");
+        }
+        else{
+            console.log("not even number");
+        }
+    }
+}

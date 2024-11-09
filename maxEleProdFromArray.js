e8627c79cd15962f38ebdb370a42052c7d8f0a0f
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-09 08:57:02 +0530

    Update maxEleProfromArray.js

--- maxEleProdFromArray.js
+++ maxEleProdFromArray.js
@@ -1,3 +1,16 @@
 function maxProduct(nums){
+    for(let i=0;i<nums.length-1;i++){
+        let maxIndex=i;
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[j]>maxIndex[j]){
+                maxIndex=j;
+            }
+        }
+        let temp=nums[i];
+        nums[i]=maxIndex[j];
+        maxIndex[j]=temp;
+    }
+    return nums[0]*nums[1];
     
+
 }

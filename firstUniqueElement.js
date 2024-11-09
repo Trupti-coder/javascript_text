9c174cebe60fe6765c9dc484fbb095528260ce2f
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-09 21:56:53 +0530

    Update function

--- firstUniqueElement.js
+++ firstUniqueElement.js
@@ -1,3 +1,18 @@
 function firstUnique(nums){
-    
+    let counts={};
+    for(let i=0;i<nums.length;i++){
+        const num=nums[i];
+        if(counts[num]===undefined){
+            counts[num]=1;
+        }
+        else{
+            counts[num]+=1;
+        }
+    }
+    for(let i=0;i<nums.length;i++){
+        if(counts[nums[i]]===1){
+            return nums[i];
+        }
+    }
+    return -1;
 }

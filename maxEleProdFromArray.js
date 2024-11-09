6d8f84f6bfc2d6629385d9e2ac9037fe7a7c4c44
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-09 09:02:15 +0530

    Update maxProduct Function

--- maxEleProdFromArray.js
+++ maxEleProdFromArray.js
@@ -2,15 +2,16 @@ function maxProduct(nums){
     for(let i=0;i<nums.length-1;i++){
         let maxIndex=i;
         for(let j=i+1;j<nums.length;j++){
-            if(nums[j]>maxIndex[j]){
+            if(nums[j]>nums[maxIndex]){
                 maxIndex=j;
             }
         }
         let temp=nums[i];
-        nums[i]=maxIndex[j];
-        maxIndex[j]=temp;
+        nums[i]=nums[maxIndex];
+        nums[maxIndex]=temp;
     }
     return nums[0]*nums[1];
-    
-
 }
+
+let nums=[1,20,30,4,6,2,70];
+console.log(maxProduct(nums));

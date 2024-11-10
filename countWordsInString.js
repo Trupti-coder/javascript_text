81c3bdc01f27ad54e0807fb0926f1cfc060f36c8
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-10 15:36:50 +0530

    Update countWordInString.js

--- countWordsInString.js
+++ countWordsInString.js
@@ -2,6 +2,19 @@ function countWords(str){
     let count=0;
     let inWords=0;
     for(let i=0;i<str.length;i++){
+        if(str[i] !==''){
+            if(!inWords){
+                count++;
+                inWords=true;
+            }
+        }
+        else{
+            inWords=false;
+        }
+       
+        
         
     }
+    return count;
+
 }

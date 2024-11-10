fe79726e8e84ff0495e197df9c699c6de564483b
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-10 11:37:39 +0530

    Update moveZeroesToEnd.js

--- moveZeroesToEnd.js
+++ moveZeroesToEnd.js
@@ -1,3 +1,13 @@
 function moveZeroesToEnd(nums){
-    
+    let nonZeroIndex=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            let temp=nums[nonZeroIndex];
+            nums[nonZeroIndex]=nums[i];
+            nums[i]=temp;
+
+            nonZeroIndex++;
+        }
+    }
+    return nums;
 }

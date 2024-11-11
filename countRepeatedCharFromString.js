1177574735e1f37b5884f9bf1198652ae5b4ffa0
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-11 12:04:35 +0530

    Update countRepeatedCharFromString.js

--- countRepeatedCharFromString.js
+++ countRepeatedCharFromString.js
@@ -1,3 +1,17 @@
 function compressString(str){
-    
+    let compressed='';
+    let count=1;
+
+    for(let i=1;i<str.length;i++){
+        if(str[i]===str[i-1]){
+            count++;
+        }
+        else{
+            compresses +=str[i-1]+count;
+            count=1;
+        }
+    }
+    compressed += str[str.length-1]+count;
+    return compressed.length<str.length ? compressed :str;
+
 }

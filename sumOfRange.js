4beed5e0e52a9944b8eb76b85a7d1cc81ebadfc3
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-13 15:36:26 +0530

    Update sumOfRange.js

--- sumOfRange.js
+++ sumOfRange.js
@@ -1,3 +1,10 @@
 function sumOfRange(start,end){
-    
+    let sum=0;
+    for(let i=start;i<=end;i++){
+        sum+=i;   
+     }
+     return sum;
+
 }
+console.log(sumOfRange(1,5));
+console.log(sumOfRange(1,100));

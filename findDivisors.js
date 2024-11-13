ae00c05437495c998ccf00fe3226ada34cf386fe
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-13 15:31:53 +0530

    Update findDivisors.js

--- findDivisors.js
+++ findDivisors.js
@@ -1,3 +1,14 @@
 function findDivisors(n){
-    
+    let divisors=[];
+
+    for(let i=1;i*i<=n;i++){
+        if(n%i===0){
+            divisors.push(i);
+            if(i !==n/i){
+                divisors.push(n/i);
+            }
+        }
+    }
+    return divisors;
+
 }

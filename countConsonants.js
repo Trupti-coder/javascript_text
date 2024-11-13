b5a2cac2cb655c5d9dca02b4d0a040a38277d6c6
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-13 21:45:41 +0530

    Update countConsonants.js

--- countConsonants.js
+++ countConsonants.js
@@ -1,3 +1,26 @@
 function countConsonants(str){
-    
+    let countConsonants=0;
+    let vowels="aeiouAEIOU";
+
+    for(let i=0;i<str.length;i++){
+        let char=str[i];
+
+        if((char>="a" && char <="z") || (char >= "A" && char <="Z")){
+            let isVowel=false;
+            for(let j=0;j<vowels.length;j++){
+                if(char === vowels[j]){
+                    isVowel=true;
+                    break;
+
+                }
+            }
+            if(!isVowel){
+                countConsonants++;
+            }
+            
+        }
+        return countConsonants;
+
+    }
+
 }

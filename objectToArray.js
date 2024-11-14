f8eebfd6de558ccc94f776a31c38d28dafe5b7bc
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-14 22:25:05 +0530

    Update objectToArray.js

--- objectToArray.js
+++ objectToArray.js
@@ -1,3 +1,28 @@
 function objectToArray(obj){
-    
+    const result=[];
+
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)){
+            result.push([key,obj[key]]);
+        }
+    }
+    return result;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

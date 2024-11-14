ecac21f854be75f66138ae462b55850995cc231b
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-14 22:13:05 +0530

    Update numExponent.js

--- numExponent.js
+++ numExponent.js
@@ -1,3 +1,10 @@
 function power(base,exponent){
-    
+    let result=1;
+    for(let i=0;i<exponent;i++){
+        result=result*base;
+    }
+    return result;
+
 }
+console.log(power(2,3));
+console.log(power(5,5));

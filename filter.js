e42de94d87ba33a680c897511b192b68292d6dcf
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-15 20:34:46 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,3 +1,5 @@
 const number=[1,2,3,4,5,6,7];
 const greaterThanTwo=number.filter((num)=>num>2);
+const lessThanSix=number.filter((num)=>num<6);
 console.log(greaterThanTwo);
+console.log(lessThanSix);

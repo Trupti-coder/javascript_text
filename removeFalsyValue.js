0a24ae86e273b11d04e6216fa2ca895212f7e3d8
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-15 22:50:02 +0530

    Create removeFalsyValue.js

---
+++ removeFalsyValue.js
@@ -0,0 +1,11 @@
+function removeFalsyValue(arr){
+    const result=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+
+}
+

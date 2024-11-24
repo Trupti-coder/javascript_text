0137c5f0b97f67d2896c1f7c06f6312971e50ad5
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-24 08:13:32 -0500

    Update charPyramidPattern.js

---
+++ Pattern Program/charPyramidPattern.js
@@ -0,0 +1,16 @@
+let n=5;
+let charCode=97;
+for(let i=1;i<=n;i++){
+    let line="";
+
+    for(let j=0;j<n-i;j++){
+        line+=" ";
+    }
+    for(let j=0;j<2*i-1;j++){
+        line += String.fromCharCode(charCode);
+        charCode++;
+        if(charCode>122) charCode=97;
+
+    }
+    console.log(line);
+}

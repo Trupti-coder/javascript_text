8855557020685ef5755b8930cd0ce51b436068b8
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-25 10:48:14 -0500

    Update convertStringFirstWordCap.js

--- Pattern Program/convertStringFirstWordCap.js
+++ Pattern Program/convertStringFirstWordCap.js
@@ -1,3 +1,27 @@
 function capitlizeString(str){
-    
+    let result="";
+    let capitlizeNext=true;
+
+    for(let i=0;i<str.length;i++){
+        let char =str[i];
+
+        if(char === ' '){
+            result += char;
+            capitlizeNext =true;
+        }
+        else{
+            if(capitlizeNext && char >= 'a' && char <= 'z'){
+                result += String.fromCharCode(char.charCodeAt(0)-32);
+                capitlizeNext=false;
+            }
+            else{
+                result+=char;
+                capitlizeNext=false;
+            }
+        }
+    }
+    return result;
+
 }
+let str = "hello from javascript mesh !"; 
+console.log(capitlizeString(str));

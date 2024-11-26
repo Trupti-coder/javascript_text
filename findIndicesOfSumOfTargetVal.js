57d193a4b9d85711ae944b65cdf382f12f42e3b0
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-26 07:05:57 -0500

    Update findIndicesOfSumOfTargetVal.js

--- findIndicesOfSumOfTargetVal.js
+++ findIndicesOfSumOfTargetVal.js
@@ -1,3 +1,13 @@
 function findIndices(arr,target){
-    
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]===target){
+                return [i,j];
+            }
+        }
+    }
+
 }
+let arr=[1,4,6,9,3];
+let target=12;
+console.log(findIndices(arr,target));

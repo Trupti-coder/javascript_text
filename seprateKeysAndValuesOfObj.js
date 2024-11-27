e9eebae9b9ba6d30a5594bb4e6d4b7f554a2833f
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-11-27 09:26:15 -0500

    Update seprateKeyAndValuesOfObj.js

--- seprateKeysAndValuesOfObj.js
+++ seprateKeysAndValuesOfObj.js
@@ -1,3 +1,20 @@
 function seprateKeysAndValues(){
-    
+    let keys=[];
+    let values=[];
+    let index=[];
+
+    let properties=['name','age','city'];
+
+    for(let i=0;i<properties.length;i++){
+        let key=properties[i];
+        if(obj[key] !== undefined){
+            keys[index]=key;
+            values[index]=obj[keys];
+            index++;
+        }
+    }
+    return{
+        keys:keys,
+        values:values
+    };
 }

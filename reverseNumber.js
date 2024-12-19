d16998d9cddb7bb2b0a2456c8eac743cf0662d6c
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-19 09:28:32 -0500

    Update reverseNumber.js

--- reverseNumber.js
+++ reverseNumber.js
@@ -1,1 +1,19 @@
+function reverseNumber(number) {
+    let reversed = 0;
+
+    while (number > 0) {
+        // Extract the last digit
+        let lastDigit = number % 10;
+
+        // Build the reversed number
+        reversed = reversed * 10 + lastDigit;
+
+        // Remove the last digit from the original number
+        number = (number - lastDigit) / 10;
+    }
+
+    return reversed;
+}
+
+
 

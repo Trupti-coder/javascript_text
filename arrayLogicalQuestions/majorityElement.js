2e20cdf28fc4064fd5a4eddb0abdc5cc5ddcbd4c
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-21 07:20:59 -0500

    Update

--- arrayLogicalQuestions/majorityElement.js
+++ arrayLogicalQuestions/majorityElement.js
@@ -1,3 +1,11 @@
 function majorityElement(arr){
+    let count = 0, candidate = null;
+    for (let i = 0; i < arr.length; i++) {
+        if (count === 0) {
+            candidate = arr[i];
+        }
+        count += (arr[i] === candidate) ? 1 : -1;
+    }
+    count = 0;
     
 }

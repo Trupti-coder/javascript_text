0b54117b58d67a75f49e40a43b89c65d93a4843e
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-21 08:31:48 -0500

    Update

--- arrayLogicalQuestions/intersectionOfArray.js
+++ arrayLogicalQuestions/intersectionOfArray.js
@@ -2,8 +2,13 @@ function intersectionOfArray(arr1,arr2){
     let result=[];
     for(let i=0;i<arr1.length;i++){
         for(let j=0;j<arr2.length;j++){
-            
+            if(arr1[i]===arr2[j]){
+                result.push(arr1[i]);
+                 arr2[j]=null;
+                 break;
+            }
+
         }
     }
-
+return result;
 }

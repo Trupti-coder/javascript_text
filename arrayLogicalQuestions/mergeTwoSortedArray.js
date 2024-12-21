9f4cea2878e1f0abc630ece2371ec447aada2871
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-21 08:14:47 -0500

    Update mergeTwoSortedArray.js

--- arrayLogicalQuestions/mergeTwoSortedArray.js
+++ arrayLogicalQuestions/mergeTwoSortedArray.js
@@ -3,9 +3,20 @@ function mergeSortedArray(arr1,arr2){
     let i=0; let j=0;let k=0;
     for(i<arr1.length;i++ && j<arr2.length;j++){
         if(arr1[i]<arr2[j]){
-            
+            merged[k++]=arr1[i++];
 
         }
+        else{
+            merged[k++]=arr2[j++];
+        }
+    }
+    while(i<arr1.length){
+        merged[k++]=arr1[i++];
     }
 
+    while(j<arr2.length){
+        merged[k++]=arr2[i];
+    }
+
+    return merged;
 }

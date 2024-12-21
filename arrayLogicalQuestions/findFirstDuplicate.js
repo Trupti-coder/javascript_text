f38db2504b7cd94a2745a717c67f395248778d90
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-21 07:11:48 -0500

    Update

--- arrayLogicalQuestions/findFirstDuplicate.js
+++ arrayLogicalQuestions/findFirstDuplicate.js
@@ -1,3 +1,11 @@
-function findFirstDuplicate(){
-    
+function findFirstDuplicate(arr){
+    let seen={};
+    for(let i=0;i<arr.length;i++){
+        if(seen[arr[i]]){
+            return arr[i];
+        }
+        seen[arr[i]]=true;
+    }
+    return -1;
+
 }

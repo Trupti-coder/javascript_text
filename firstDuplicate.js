8a0f2dc409a4d71c9ef76d405eee4df8c8d33781
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-27 09:11:55 -0500

    Update

--- firstDuplicate.js
+++ firstDuplicate.js
@@ -1,3 +1,11 @@
 function findFirstDuplicate(){
-    
+    let seen = {}; // Object to store seen elements
+    for (let i = 0; i < arr.length; i++) {
+        if (seen[arr[i]]) {
+            return arr[i]; // Return the first duplicate
+        }
+        seen[arr[i]] = true; // Mark the element as seen
+    }
+    return -1;
+
 }

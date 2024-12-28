6099413b6e78010e43ae5a09497fabf84792b0a5
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-27 19:54:32 -0500

    Update generateKeyValueArray.js

--- generateKeyValueArray.js
+++ generateKeyValueArray.js
@@ -1,3 +1,11 @@
-function generateKeyValueArray(){
+function generateKeyValueArray(obj){
+    let keys=[];
+    let values=[];
+
+    for(let key in obj){
+        keys[keys.length]=key;
+        values[values.length]=obj[key];
+    }
+    return {keys:keys,values:values};
 
 }

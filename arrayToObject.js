a7a9e79a12be4e66b2192d90ce28050eab86c526
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-28 22:38:51 -0500

    Update

--- arrayToObject.js
+++ arrayToObject.js
@@ -1,3 +1,9 @@
-function arrayToObject(){
-    
+function arrayToObject(arr){
+
+    let result = {};
+    for (let i = 0; i < arr.length; i++) {
+        let key = arr[i].id;
+        result[key] = arr[i];
+    }
+    return result;
 }

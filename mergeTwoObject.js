c34a1ad319735828191d82964c34e208612d8a19
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-28 22:43:33 -0500

    update

--- mergeTwoObject.js
+++ mergeTwoObject.js
@@ -4,4 +4,14 @@ function mergeTwoObject(obj1,obj2){
     for (let key in obj1) {
         result[key] = obj1[key];
     }
+
+
+    for (let key in obj2) {
+        if (result[key] !== undefined) {
+            result[key] += obj2[key];
+        } else {
+            result[key] = obj2[key];
+        }
+    }
+    return result;
 }

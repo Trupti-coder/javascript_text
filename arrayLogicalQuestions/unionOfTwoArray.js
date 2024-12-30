1b3c89f8d7ad4c20332110af5a4d6efee4af3fd2
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-30 12:30:56 -0500

    Update

--- arrayLogicalQuestions/unionOfTwoArray.js
+++ arrayLogicalQuestions/unionOfTwoArray.js
@@ -1,9 +1,16 @@
 function findUnion(arr1,arr2){
     let result=[];
+    
     for(let i=0;i<arr1.length;i++){
         if(!result.includes(arr1[i])){
-            result.push(arr[i]);
+            result.push(arr1[i]);
         }
     }
-    
+
+    for(let j=0;j<arr2.length;j++){
+        if(!result.includes(arr2[j])){
+            result.push(arr2[j]);
+        }
+    }
+
 }

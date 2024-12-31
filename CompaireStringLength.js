f7abda07344dfc841fdeef2740de851849b9ecee
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-30 19:28:40 -0500

    Update maxSubarraySum.js

--- CompaireStringLength.js
+++ CompaireStringLength.js
@@ -14,35 +14,5 @@ function compareStringCount(str1,str2){
 console.log(compareStringCount("ab","abcc"));
 
 
-/*
-
-//practice
-
-function compareStringLength(str1,str2){
-    len1=str1.length;
-    len2=str2.length;
-    if(len1===len2){
-        return " Length Are Equal";
-    }
-    else if(len1<len2){
-        return `length of ${str1} is shorter than ${str2}`;
-    }
-    else{
-        return `length of ${str1} is greater than ${str2}`;
-    }
-}
-//console.log(compareStringLength("truptii","Rani"));
-//console.log(compareStringLength("trup","Rani"));
-console.log(compareStringLength("trup","Raniii"))*/
 
 
-//fibonasi series
-
-function fibonasiSeries(n){
-    let fibo=[0,1];
-    for(let i=2;i<n;i++){
-     fibo[i]=fibo[i-1]+fibo[i-2];
-    }
-    return fibo.slice(0,n);
-}
-console.log(fibonasiSeries(10));
--- arrayLogicalQuestions/maxSubarraySum.js
+++ arrayLogicalQuestions/maxSubarraySum.js
@@ -1,5 +1,6 @@
 function maxSubarraySum(arr) {
-    let maxSum = -Infinity, currentSum = 0;
+    let maxSum = -Infinity,
+     currentSum = 0;
     for (let i = 0; i < arr.length; i++) {
         currentSum = Math.max(arr[i], currentSum + arr[i]);
         maxSum = Math.max(maxSum, currentSum);

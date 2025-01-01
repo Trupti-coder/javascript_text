d80b13751e6f19375af8ec6ba62c10e3b8285dd1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-01 18:18:31 -0500

    Update

--- objectLogicalQue/getNestedProperty.js
+++ objectLogicalQue/getNestedProperty.js
@@ -1,3 +1,12 @@
-function getNestedProperty(){
-    
+function getNestedProperty(obj,keys){
+    let current = obj;
+    for (let i = 0; i < keys.length; i++) {
+        if (current[keys[i]] !== undefined) {
+            current = current[keys[i]];
+        } else {
+            return undefined; // Return undefined if the property doesn't exist
+        }
+    }
+    return current;
+
 }

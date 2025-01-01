0383a58093246515105685dd16339f3ae6f5711c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-01 18:23:02 -0500

    Update

--- objectLogicalQue/objectToJSON.js
+++ objectLogicalQue/objectToJSON.js
@@ -1,3 +1,10 @@
 function stringify(obj){
-    
+    let result = '{';
+    for (let key in obj) {
+        let value = typeof obj[key] === 'string' ? `"${obj[key]}"` : obj[key];
+        result += `"${key}":${value},`;
+    }
+    result = result.slice(0, -1); // Remove trailing comma
+    result += '}';
+    return result;
 }

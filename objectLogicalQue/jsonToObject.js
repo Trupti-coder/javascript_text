0c4ab242567f6b5bf9122a32e4b3debd261a1fdc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-01 18:27:19 -0500

    Update jsonToObject.js

--- objectLogicalQue/jsonToObject.js
+++ objectLogicalQue/jsonToObject.js
@@ -2,3 +2,8 @@ function parseJSON(){
     return eval('(' + json + ')');
 
 }
+
+
+// Example
+let jsonString = '{"name":"Alice","age":25}';
+console.log(parseJSON(jsonString)); // { name: "Alice", age: 25 }

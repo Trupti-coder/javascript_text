7be5e900d50d075d76ad6d8e5312feaaa741d5be
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-01 18:31:18 -0500

    Update objectWithoutPrototype.js

--- objectLogicalQue/objectWithoutPrototype.js
+++ objectLogicalQue/objectWithoutPrototype.js
@@ -1,3 +1,9 @@
 function plainObject(){
     return Object.create(null);
 }
+
+// Example
+let obj = createPlainObject();
+obj.name = "Alice";
+console.log(obj); // { name: "Alice" }
+console.log(Object.getPrototypeOf(obj)); // null

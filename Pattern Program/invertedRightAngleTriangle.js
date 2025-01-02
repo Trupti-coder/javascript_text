52ce1d400cf0000d91c3400d2fc8c319f4a895fd
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-02 14:35:16 -0500

    update

--- Pattern Program/invertedRightAngleTriangle.js
+++ Pattern Program/invertedRightAngleTriangle.js
@@ -1,4 +1,10 @@
 function invertedTriangle(rows){
-  
+    for (let i = rows; i >= 1; i--) {
+        let str = "";
+        for (let j = 1; j <= i; j++) {
+            str += "*";
+        }
+        console.log(str);
+    }
 
 }

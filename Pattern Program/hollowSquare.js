f0ae71bff56e38506853be652157852d958ca234
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-02 14:46:08 -0500

    update

--- Pattern Program/hollowSquare.js
+++ Pattern Program/hollowSquare.js
@@ -1,3 +1,13 @@
 function hollowSquare(rows){
-    
+    for (let i = 1; i <= rows; i++) {
+        let str = "";
+        for (let j = 1; j <= rows; j++) {
+            if (i === 1 || i === rows || j === 1 || j === rows) {
+                str += "*";
+            } else {
+                str += " ";
+            }
+        }
+        console.log(str);
+    }
 }

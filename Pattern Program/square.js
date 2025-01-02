b6de1b3d172ffaf5c2b13a09c126191242ab4852
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-02 14:39:43 -0500

    update square.js

--- Pattern Program/square.js
+++ Pattern Program/square.js
@@ -1,3 +1,11 @@
-function square(){
-    
+function square(rows){
+
+    for (let i = 1; i <= rows; i++) {
+        let str = "";
+        for (let j = 1; j <= rows; j++) {
+            str += "*";
+        }
+        console.log(str);
+    }
+
 }

758e2effe42f44334d07f98c5eb41525d2101f57
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-02 14:27:08 -0500

    Update triangle.js

---
+++ Pattern Program/triangle.js
@@ -0,0 +1,10 @@
+function rightAngledTriangle(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let str = "";
+        for (let j = 1; j <= i; j++) {
+            str += "*";
+        }
+        console.log(str);
+    }
+}
+rightAngledTriangle(5);

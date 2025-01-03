21fc2ff73880fa5f7022efe2e0978dabaff41f21
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-03 14:45:08 -0500

    Update

--- arrayLogicalQuestions/majorityElement.js
+++ arrayLogicalQuestions/majorityElement.js
@@ -1,22 +1,3 @@
-function majorityElement(arr){
-    let count = 0, candidate = null;
-    for (let i = 0; i < arr.length; i++) {
-        if (count === 0) {
-            candidate = arr[i];
-        }
-        count += (arr[i] === candidate) ? 1 : -1;
-    }
-    count = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === candidate) {
-            count++;
-        }
-    }
-    return count > arr.length / 2 ? candidate : null;
+function findMajorityElementInArray(){
     
 }
-
-// Input
-let arr6 = [3, 3, 4, 2, 4, 4, 2, 4, 4];
-console.log(majorityElement(arr6)); // Output: 4

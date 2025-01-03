589f183ed6fc7d7a512071bff12b5d8addfac208
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-03 14:54:31 -0500

    update

--- arrayLogicalQuestions/majorityElement.js
+++ arrayLogicalQuestions/majorityElement.js
@@ -2,4 +2,15 @@ function findMajorityElementInArray(arr){
     let maxCount = 0; // To keep track of the maximum count
     let majorityElement = null; // To store the majority element
 
+     // Outer loop to pick each element
+     for (let i = 0; i < arr.length; i++) {
+        let count = 0; // Reset count for each element
+
+          // Inner loop to count occurrences of the selected element
+          for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                count++; // Increment count when a match is found
+            }
+        }
+
 }

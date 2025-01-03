96fd6a919f10e1184b00a84281457944ac1a85b1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-03 10:50:26 -0500

    Update

--- arrayLogicalQuestions/mergeTwoSortedArray.js
+++ arrayLogicalQuestions/mergeTwoSortedArray.js
@@ -3,4 +3,15 @@ function mergeSortedArray(arr1,arr2){
     let i = 0; // Pointer for arr1
     let j = 0; // Pointer for arr2
 
+     // Compare elements from both arrays and add the smaller one to the merged array
+     while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
 }

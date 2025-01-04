d13bd37748cba520aafeed7b2541fefd55d32a11
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-04 12:06:12 -0500

    Update chunkArray.js

--- arrayLogicalQuestions/chunkArray.js
+++ arrayLogicalQuestions/chunkArray.js
@@ -1,5 +1,14 @@
 //chunk means spliting an array into  smaller group of size k 
 
-function chunkArray(){
-    
+function chunkArray(arr,k){
+
+    let result=[]; // for storing the chunks of ann array
+    let chunk=[]; // for storing the current chunk
+
+    for(let i=0;i<arr.length;i++){
+        chunk.push(arr[i]);
+    }
+
+
+
 }

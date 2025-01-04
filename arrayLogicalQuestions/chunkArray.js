1a4f8e7c817c8f98550b03d3b914a749e7e26737
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-04 12:09:32 -0500

    Update

--- arrayLogicalQuestions/chunkArray.js
+++ arrayLogicalQuestions/chunkArray.js
@@ -5,9 +5,16 @@ function chunkArray(arr,k){
     let result=[]; // for storing the chunks of ann array
     let chunk=[]; // for storing the current chunk
 
-    for(let i=0;i<arr.length;i++){
+    for(let i=0;i<arr.length;i++){    // function ueses for loop for going through each element in an array
         chunk.push(arr[i]);
     }
+
+    if(chunk.size===k){         // if chunk size is equal to the desired size then push chunk into the result.
+        result.push(chunk);
+        chunk=[];
+    }
+
+
 
 
 

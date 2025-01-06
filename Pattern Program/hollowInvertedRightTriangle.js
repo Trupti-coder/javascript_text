63e39cdaa1689509a324d0cd24463890a7de468c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-05 19:27:34 -0500

    Update hollowInvertedRightTriangle.js

--- Pattern Program/hollowInvertedRightTriangle.js
+++ Pattern Program/hollowInvertedRightTriangle.js
@@ -1,3 +1,16 @@
 function hollowInvertedRightTriangle(){
-    
+
+    for (let i = rows; i >= 1; i--) {
+        let str = '';
+        for (let j = 1; j <= i; j++) {
+            if (j === 1 || j === i || i === rows) {
+                str += '*';
+            } else {
+                str += ' ';
+            }
+        }
+        console.log(str);
+    }
+
 }
+

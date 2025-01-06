d37294bd98b71e70d1e4216ddcffb1cc7cdba9df
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-05 19:31:10 -0500

    update

--- Pattern Program/rightPascalsTriangle.js
+++ Pattern Program/rightPascalsTriangle.js
@@ -1,3 +1,11 @@
 function rightPascalsTriangle(rows){
-    
+    // Upper part
+    for (let i = 1; i <= rows; i++) {
+        let str = '';
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        console.log(str);
+    }
+
 }

addf9763d40c5f091ea70527d8ac89461e361674
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-07 11:16:07 -0500

    Update

--- Pattern Program/butterfly.js
+++ Pattern Program/butterfly.js
@@ -1,8 +1,16 @@
-function butterfly(){
+function butterfly(rows){
   for(let i=1;i<=rows;i++){
     let str='';
     for(let j=1;j<=i;j++){
         str+='*';
       }
-  }  
+      for(let k=1;k<=(2*rows-2*i); k++){
+        str+='';
+      }
+      for(let j=1;j<=i;j++){
+        str+="*";
+      }
+      console.log(str);
+  } 
+
 }

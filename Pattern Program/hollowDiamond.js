8da95c9fafec3cf50003e224ed17c1db3b3d4462
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-06 22:05:12 -0500

    Update

--- Pattern Program/hollowDiamond.js
+++ Pattern Program/hollowDiamond.js
@@ -5,4 +5,11 @@ function hollowDiamond(){
         for (let j = 1; j <= rows - i; j++) {
             str += ' ';
         }
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            if (k === 1 || k === (2 * i - 1)) {
+                str += '*';
+            } else {
+                str += ' ';
+            }
+        }
 }

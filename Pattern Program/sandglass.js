26afbf577125ee9158b6bf134c5e22ccaf1fe77e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-06 22:11:38 -0500

    Update

--- Pattern Program/sandglass.js
+++ Pattern Program/sandglass.js
@@ -1,3 +1,8 @@
-function sandglass(){
-    
+function sandglass(rows){
+     // Upper part
+     for (let i = rows; i >= 1; i--) {
+        let str = '';
+        for (let j = 1; j <= rows - i; j++) {
+            str += ' ';
+        }
 }

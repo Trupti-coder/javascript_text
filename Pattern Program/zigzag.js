692cf54c7130754599b242d0ef7cfdb351945572
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-06 22:23:01 -0500

    Update

--- Pattern Program/zigzag.js
+++ Pattern Program/zigzag.js
@@ -3,4 +3,14 @@ function zigzag(rows){
     for (let i = 1; i <= 3; i++) {
         let str = '';
 
+        for (let j = 1; j <= rows; j++) {
+            if (((i + j) % 4 === 0) || (i === 2 && j % 4 === 0)) {
+                str += '*';
+            } else {
+                str += ' ';
+            }
+        }
+        console.log(str);
+    }
+
 }

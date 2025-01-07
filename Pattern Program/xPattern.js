8e16df46309501549bd1278b0389adf909d0a528
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-07 11:26:41 -0500

    Update xPattern.js

--- Pattern Program/xPattern.js
+++ Pattern Program/xPattern.js
@@ -1,3 +1,13 @@
 function xPattern(rows){
-    
+    for(let i=1;i>=rows;i++){
+        let str='';
+        for(let j=1;j<=rows;j++){
+            if(j === i || j === (rows-i+1)){
+                str+="*";
+            }
+            else{
+                str += ' ';
+            }
+        }
+    }
 }

9c5c467bde4ecb15651897aa350d78ecb6913b9f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-08 08:39:38 -0500

    Update

--- Pattern Program/hexagon.js
+++ Pattern Program/hexagon.js
@@ -1,3 +1,13 @@
 function hexagon(){
+    for(let i=1;i<=rows;i++){
+        let str = '';
+        for (let j = 1; j <= rows - i; j++) {
+            str += ' ';
+        }
+        for (let k = 1; k <= rows + (i - 1); k++) {
+            str += '*';
+        }
+        
+    }
 
 }

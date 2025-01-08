9e1bbf16c0346c2a7c74b8e93c34ff1b32104e31
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-07 23:09:46 -0500

    Update

--- Pattern Program/rightArrow.js
+++ Pattern Program/rightArrow.js
@@ -1,3 +1,8 @@
 function rightArrow(){
-    
+    for (let i = 1; i <= rows; i++) {
+        let str = '';
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        console.log(str);
 }

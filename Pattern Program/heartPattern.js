9abc46b139e0bed781222e2bc78e5483adb1778b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-08 08:32:39 -0500

    Update

--- Pattern Program/heartPattern.js
+++ Pattern Program/heartPattern.js
@@ -1,3 +1,9 @@
 function heartPattern(){
-    
+    let n=rows/2;
+    for(let i = n / 2; i <= n; i += 2){
+        let str = '';
+        for (let j = 1; j < n - i; j += 2) {
+            str += ' ';
+        }
+    }
 }

7a6b610de015cc550dfa64526ee00e4bd20dd2b4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-09 11:04:22 -0500

    Update

--- Pattern Program/hourglassOutline.js
+++ Pattern Program/hourglassOutline.js
@@ -1,33 +1,31 @@
-function hourglassOutline(){
+function hourglassOutline(rows) {
     for (let i = rows; i >= 1; i--) {
         let str = '';
         for (let j = 1; j <= rows - i; j++) {
             str += ' ';
         }
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            if (k === 1 || k === (2 * i - 1) || i === rows) {
+                str += '*';
+            } else {
+                str += ' ';
+            }
+        }
+        console.log(str);
     }
-    for (let k = 1; k <= (2 * i - 1); k++) {
-        if (k === 1 || k === (2 * i - 1) || i === rows) {
-            str += '*';
-        } else {
+    for (let i = 2; i <= rows; i++) {
+        let str = '';
+        for (let j = 1; j <= rows - i; j++) {
             str += ' ';
         }
-    }
-    console.log(str);
-}
-for (let i = 2; i <= rows; i++) {
-    let str = '';
-    for (let j = 1; j <= rows - i; j++) {
-        str += ' ';
-    }
-
-    for (let k = 1; k <= (2 * i - 1); k++) {
-        if (k === 1 || k === (2 * i - 1)) {
-            str += '*';
-        } else {
-            str += ' ';
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            if (k === 1 || k === (2 * i - 1)) {
+                str += '*';
+            } else {
+                str += ' ';
+            }
         }
+        console.log(str);
     }
-    console.log(str);
 }
-
-
+hourglassOutline(5);

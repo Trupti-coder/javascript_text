225b33387541485300c2f6e428ffa43e680a7c8c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-09 11:00:38 -0500

    update

--- Pattern Program/hourglassOutline.js
+++ Pattern Program/hourglassOutline.js
@@ -19,5 +19,15 @@ for (let i = 2; i <= rows; i++) {
     for (let j = 1; j <= rows - i; j++) {
         str += ' ';
     }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        if (k === 1 || k === (2 * i - 1)) {
+            str += '*';
+        } else {
+            str += ' ';
+        }
+    }
+    console.log(str);
 }
+
 

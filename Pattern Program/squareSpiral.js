5dfaaa451704aca95e9b6b2193035cee686964c9
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-09 10:50:24 -0500

    Update squareSpiral.js

--- Pattern Program/squareSpiral.js
+++ Pattern Program/squareSpiral.js
@@ -1,4 +1,4 @@
-function squareSpiral(){
+function squareSpiral(n){
     let matrix = Array.from({ length: n }, () => Array(n).fill(' '));
     let top = 0, bottom = n - 1, left = 0, right = n - 1;
     let count = 1;
@@ -33,3 +33,4 @@ function squareSpiral(){
         console.log(matrix[i].join(''));
     }
 }
+squareSpiral(7);

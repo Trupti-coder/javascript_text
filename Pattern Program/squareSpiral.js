e6668b0e052b7899a1aab77067871a665f7a95b8
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-09 10:47:44 -0500

    Update

--- Pattern Program/squareSpiral.js
+++ Pattern Program/squareSpiral.js
@@ -2,4 +2,10 @@ function squareSpiral(){
     let matrix = Array.from({ length: n }, () => Array(n).fill(' '));
     let top = 0, bottom = n - 1, left = 0, right = n - 1;
     let count = 1;
+
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            matrix[top][i] = '*';
+        }
+        top++;
 }

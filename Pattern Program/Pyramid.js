04dcebd298ac33cdd501a27262b872a8a43c7239
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-10 15:41:47 -0500

    Update

--- Pattern Program/Pyramid.js
+++ Pattern Program/Pyramid.js
@@ -1,3 +1,11 @@
 function  doublePyramid(){
-    
+    for(let i=0;i<=rows;i++){
+        let str = '';
+        for (let j = 1; j <= rows - i; j++) {
+            str += ' ';
+        }
+
+
+    }
+
 }

1ff06cc15ae97afef082ee01a6425e291d3ae3eb
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-10 15:39:11 -0500

    Update crossPattern.js

--- Pattern Program/crossPattern.js
+++ Pattern Program/crossPattern.js
@@ -1,4 +1,4 @@
-function crossPattern(){
+function crossPattern(rows){
     for (let i = 1; i <= rows; i++) {
         let str = '';
         for (let j = 1; j <= rows; j++) {
@@ -12,3 +12,4 @@ function crossPattern(){
     }
 
 }
+crossPattern(7);

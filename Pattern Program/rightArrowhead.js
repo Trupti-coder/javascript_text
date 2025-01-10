4c05fa08671e4144a241f12db0a92b1170e8a991
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-10 15:32:54 -0500

    Update

--- Pattern Program/rightArrowhead.js
+++ Pattern Program/rightArrowhead.js
@@ -1,3 +1,9 @@
 function rightArrowhead(){
-    
+    for (let i = 1; i <= rows; i++) {
+        let str = '';
+        for (let j = 1; j <= i; j++) {
+            str += '*';
+        }
+        console.log(str);
+    }
 }

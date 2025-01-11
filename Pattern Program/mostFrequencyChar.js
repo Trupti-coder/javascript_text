c673d58e9e25719a7e423ce0052d7f27af0d3fa3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-11 12:09:53 -0500

    Update

--- Pattern Program/mostFrequencyChar.js
+++ Pattern Program/mostFrequencyChar.js
@@ -1,3 +1,11 @@
 function mostFreqChar(){
-    
+    let freq={};
+    for(let i=0;i<str.length;i++){
+        if(!freq[str[i]]){
+            freq[str[i]]=1;
+        }
+        else{
+            freq[str[i]]++;
+        }
+    }
 }

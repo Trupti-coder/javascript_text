e7ac3560a546b946cda19dbad25cec18229994be
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-12 11:49:34 -0500

    Update longestConsecutiveSequence.js

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,8 +1,21 @@
-function longestConsecutiveSequence(){
+function longestConsecutiveSequence(nums){
     let set = {};
     for (let num of nums) {
         set[num] = true;
     }
 
     let maxLength = 0;
+
+
+    for (let num of nums) {
+        if (!set[num - 1]) {
+            let length = 0;
+            while (set[num + length]) {
+                length++;
+            }
+            maxLength = Math.max(maxLength, length);
+        }
+    }
+
+    return maxLength;
 }

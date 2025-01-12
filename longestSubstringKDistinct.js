be155dc7e4a97c168e85b6ce1ecbccfc587cce10
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-11 21:40:45 -0500

    Update

--- longestSubstringKDistinct.js
+++ longestSubstringKDistinct.js
@@ -1,3 +1,12 @@
 function longestSubstringKDistinct(){
-    
+    let maxLength = 0;
+    let start = 0;
+    let charCount = {};
+
+    for (let end = 0; end < s.length; end++) {
+        let char = s[end];
+        if (!charCount[char]) {
+            charCount[char] = 0;
+        }
+        charCount[char]++;
 }

b656774d01a62af8f08bbcb78b13678b67a7ba71
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-12 11:56:05 -0500

    Update subarraySum.js

--- subarraySum.js
+++ subarraySum.js
@@ -1,4 +1,4 @@
-function subarraySum(){
+function subarraySum(nums,k){
     let count = 0;
     let sum = 0;
     let sumMap = { 0: 1 };
@@ -8,5 +8,11 @@ function subarraySum(){
         if (sumMap[sum - k]) {
             count += sumMap[sum - k];
         }
+
+        if (!sumMap[sum]) {
+            sumMap[sum] = 0;
+        }
+        sumMap[sum]++;
     }
+    return count;
 }

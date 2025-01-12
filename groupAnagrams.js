9598caf6bf5667de399488a854477fc3006e530f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-12 11:41:53 -0500

    update

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,5 +1,12 @@
-function groupAnagrams(){
+function groupAnagrams(strs){
     let group={};
+
+    for(let str of strs){
+        let count = Array(26).fill(0);
+        for (let char of str) {
+            count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+    }
 
    
 }

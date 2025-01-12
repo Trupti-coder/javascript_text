cfe910f133827c8cf6ed711969b219e3a395fd8c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-12 11:45:18 -0500

    Update groupAnagrams.js

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,12 +1,23 @@
 function groupAnagrams(strs){
-    let group={};
+    let groups={};
 
     for(let str of strs){
         let count = Array(26).fill(0);
         for (let char of str) {
             count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
         }
+
+        let key = count.join('#');
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+        groups[key].push(str);
     }
 
+    return Object.values(groups);
+    }
+
+    console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
+
    
-}

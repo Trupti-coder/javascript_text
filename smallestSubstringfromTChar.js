161eb5841a261115c02f8ba2c429a1e0ee1ba344
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-11 21:51:18 -0500

    Update

--- smallestSubstringfromTChar.js
+++ smallestSubstringfromTChar.js
@@ -3,4 +3,11 @@ function smallestSubstringTChar(){
     let minStart = 0;
     let start = 0;
     let charCount = {};
+
+    for (let i = 0; i < t.length; i++) {
+        if (!charCount[t[i]]) {
+            charCount[t[i]] = 0;
+        }
+        charCount[t[i]]++;
+    }
 }

41ec5f4fc6feaa3df9f50d27d411cf389b78fd11
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-13 10:39:24 -0500

    Update

--- minDistance.js
+++ minDistance.js
@@ -1,3 +1,6 @@
 function minDistance(){
-    
+    let dp = Array(word1.length + 1).fill().map(() => Array(word2.length + 1).fill(0));
+    for (let i = 0; i <= word1.length; i++){
+        
+    }
 }

6431546d1fec685cd13462b9795f5f31c2110990
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-13 21:57:08 -0500

    update

--- longestValidParentheses.js
+++ longestValidParentheses.js
@@ -1,3 +1,12 @@
 function longestValidParentheses(){
-    
+    let stack = [-1];
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++){
+        if (s[i] === '(') {
+            stack.push(i);
+        } else {
+            stack.pop();
+
+    }
 }

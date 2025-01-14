e7a0eac73586262b107e635e02496799210afbe4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-13 21:52:39 -0500

    update

--- permutation.js
+++ permutation.js
@@ -2,7 +2,15 @@ function permutation(){
     let results = [];
     function permute(arr, m = []) {
         if (arr.length === 0){
-            
+            results.push(m.join(''));
+
+        }
+        else{
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr, m.concat(next));
+            }
         }
     }
 }
